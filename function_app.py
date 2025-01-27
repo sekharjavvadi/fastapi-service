5615edde92054825c939322d6ce4562f322fe433
varanasisainarayana <karthiks1mail@gmail.com>
2025-01-27 19:43:34 +0530

    convert get api as post api

--- function_app.py
+++ function_app.py
@@ -4,25 +4,24 @@ from fast_api import analyze_video_endpoint
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger")
+@app.route(route="http_trigger", methods=["POST"])
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    video_url = req.params.get('video_url')
-    if not video_url:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            video_url = req_body.get('video_url')
+    try:
+        req_body = req.get_json()
+        video_url = req_body.get('video_url', None)
+    except ValueError:
+        return func.HttpResponse(
+            "Invalid request body. Please provide a valid JSON object.",
+            status_code=400
+        )
 
     if video_url:
-        result=analyze_video_endpoint(video_url)
-        # return func.HttpResponse(result)
+        result = analyze_video_endpoint(video_url)
         return func.HttpResponse(f"{result}")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a video_url in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Please provide a 'video_url' in the request body.",
+            status_code=400
         )
