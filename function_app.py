0b19becefe7b87780f57cc6cc1350a3aa409515a
Alisettirajitha <rajithaalisetti@spotmies.ai>
2025-02-20 11:33:05 +0530

    multivoice integration added

--- function_app.py
+++ function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import logging
-from fast_api import analyze_video_endpoint,object_detection_endpoint,tab_shift_endpoint
+from fast_api import analyze_video_endpoint,object_detection_endpoint,tab_shift_endpoint,detect_multiple_voices_endpoint
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -80,3 +80,27 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
 #small comment
+
+@app.route(route="http_trigger_multiple_voice")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    video_url = req.params.get('video_url')
+    input_seconds = req.params.get('input_seconds')
+    if not video_url and input_seconds:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            video_url = req_body.get('video_url')
+            input_seconds = req_body.get('input_seconds')
+
+    if video_url and input_seconds:
+        result=detect_multiple_voices_endpoint(video_url,input_seconds)
+        return func.HttpResponse(f"{result}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a video_url in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
